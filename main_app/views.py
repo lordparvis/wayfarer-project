0b61a1e26935227768dd6517b29825d426c49997
gngfang <63154900+gngfang@users.noreply.github.com>
2020-06-18 15:03:13 -0400

    added signup and login

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 from .models import Profile
+
 
 
 # Create your views here.
@@ -15,3 +19,16 @@ def profile(request):
     context={'profile':profile}
     return render(request,'profile/profile.html',context)
 
+# Sign Up
+
+def signup(request):
+    if request.method == 'POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            return redirect('home')
+    else:
+        error_message='Invalid Sign Up'
+    form=UserCreationForm()
+    context={'form':form}
+    return render(request,'registration/signup.html',context)
