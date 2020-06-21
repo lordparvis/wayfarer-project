541f4e98c2e9824bb394b824ae1a4464ee14e796
gngfang <63154900+gngfang@users.noreply.github.com>
2020-06-21 00:45:21 -0400

    added edit user

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,14 +4,14 @@ from django.urls import path
 from .import views
 
 
-urlpatterns=[
-    path('',views.home, name='home'),
-    path('profile/',views.profile, name='profile'),
-    path('profile/edit/<int:profile_id>', views.profile_edit, name='edit'),
-    path('accounts/signup/',views.signup,name='signup'),
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/<int:profile_id>/edit', views.profile_edit, name='edit'),
+    path('accounts/signup/', views.signup, name='signup'),
     path('accounts/login/', views.login, name='login'),
-    path('post/',views.post, name='post'),
-    path('city/',views.city, name='city'),
-    path('city/<int:city_id>/',views.detail_city,name='detail'),
-    path('profile/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('post/', views.post, name='post'),
+    path('city/', views.city, name='city'),
+    path('city/<int:city_id>/', views.detail_city, name='detail'),
+    path('profile/<int:post_id>/', views.post_detail, name='post_detail')
 ]
